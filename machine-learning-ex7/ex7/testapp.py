f7a3cafce175157e79fe5dbe67261ec550263d7e
shan1322 <shaneyasingh13@gmail.com>
2017-12-31 00:49:42 -0800

    Quick fix

---
+++ machine-learning-ex7/ex7/testapp.py
@@ -0,0 +1,15 @@
+
+from flask_socketio import join_room, leave_room
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', room=room)
